57549d56982b2524190c61c2dfb1b11a571d7690
ikkjo <ilija.kalinic@gmail.com>
2023-01-02 22:39:39 +0100

    Backend project initialization

---
+++ SuuberBackend/src/main/java/com/suuber/Suuber/SuuberApplication.java
@@ -0,0 +1,13 @@
+package com.suuber.Suuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuuberApplication.class, args);
+	}
+
+}
---
+++ SuuberBackend/src/test/java/com/suuber/Suuber/SuuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suuber.Suuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
